246f2e923fd1923230ceb287e240317fe1cc37eb
Angelo Barredo <aibarredo@gmail.com>
2021-01-28 02:33:38 +0800

    Made PlayerObject prefab with basic move and jump

---
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField] [Range(1f, 10f)] float jumpVelocity = 2.5f;
+    [SerializeField] [Range(1f, 10f)] float fallMultiplier = 2.5f;
+    [SerializeField] [Range(1f, 10f)] float lowJumpMultiplier = 2f;
+
+    Rigidbody rb;
+    float gravityScale = 1f;
+    bool isJump;
+
+    void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isJump)
+        {
+            rb.AddForce(Vector2.up * jumpVelocity, ForceMode.Impulse);
+            isJump = false;
+        }
+
+        rb.AddForce(Physics.gravity * ((rb.velocity.y < 0) ? fallMultiplier : ((rb.velocity.y > 0 && !Input.GetButton("Jump")) ? lowJumpMultiplier : 1f)), ForceMode.Acceleration);
+
+        //if (rb.velocity.y < 0) rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        //else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump")) isJump = true;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Parameters")]
+    [SerializeField][Range(0.1f, 20f)] float playerSpeed = 3f;
+    //[SerializeField] bool checkGround = true;
+    //[SerializeField] LayerMask filterGround;
+
+    // Insert Raycast elements here
+
+    void FixedUpdate()
+    {
+        // Moves player depending on Input Axis
+        // Ideally there should be a Raycast checking if player is on the ground
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), transform.position.y, Input.GetAxis("Vertical")) * playerSpeed * Time.deltaTime;
+    }
+}
