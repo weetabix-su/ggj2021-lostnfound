2e18bdd7ace1324eca9f23a5a07d17a461385cff
hkjason <66897406+hkjason@users.noreply.github.com>
2021-01-28 23:10:34 +0800

    scene 1 navMesh, queue script

--- Assets/Scripts/AI/Queue.cs
+++ Assets/Scripts/AI/Queue.cs
@@ -5,12 +5,18 @@ using UnityEngine.AI;
 
 public class Queue : MonoBehaviour
 {
+    [Header("Settings")]
     public float waitingTime;
     public int numberOfHumans;
+    public float walkingSpeed;
 
+    [HideInInspector]
     public List<Transform> humans;
+    [HideInInspector]
     public List<NavMeshAgent> agents;
+    [HideInInspector]
     public List<Vector3> locations;
+    [Space(5)]
     public GameObject destroyCollider;
 
 
@@ -21,6 +27,7 @@ public class Queue : MonoBehaviour
         {
             humans.Add(transform.GetChild(i));
             agents.Add(humans[i].GetComponent<NavMeshAgent>());
+            agents[i].speed = walkingSpeed;
         }
         for (int i = numberOfHumans; i < numberOfHumans * 2; i++)
         {
