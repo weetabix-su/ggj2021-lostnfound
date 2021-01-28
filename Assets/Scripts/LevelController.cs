0c86f3dace23c58f0353e05d8bea26d3fba9430a
hanglch <46513241+hanglch@users.noreply.github.com>
2021-01-28 19:03:50 +0800

    minor push

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LevelController : MonoBehaviour
+{
+    [Header("UI reference and parameter")]
+    public Image fadeImg;
+    [SerializeField] [Range(0.5f, 4f)] float fadeSpd;
+
+    public void Fade(bool isFadeIn)
+    {
+        if (isFadeIn)
+        {
+            Color tempColor = Color.clear;
+
+            LeanTween.value(this.gameObject, tempColor, Color.black, fadeSpd).setEase(LeanTweenType.easeOutQuint).setOnUpdate((Color val) =>
+            {
+                fadeImg.color = val;
+            });
+        }
+        else
+        {
+            Color tempColor = Color.clear;
+
+            LeanTween.value(this.gameObject, Color.black, tempColor, fadeSpd).setEase(LeanTweenType.easeOutQuint).setOnUpdate((Color val) =>
+            {
+                fadeImg.color = val;
+            });
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Fade(true);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Fade(false);
+        }
+    }
+}
