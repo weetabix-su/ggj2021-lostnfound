a10b5f0c469522b4fd1f31cd81d3ad6d54010b96
Angelo Barredo <aibarredo@gmail.com>
2021-01-30 00:14:13 +0800

    Added option to limit player movement to specific axis

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum moveMode { xOnly, yOnly, twoDimensional }
+
 public class PlayerMove : MonoBehaviour
 {
     [Header("Parameters")]
     [SerializeField][Range(0.1f, 20f)] float playerSpeed = 3f;
+    [SerializeField] moveMode moveDirections;
     [SerializeField] bool flipX;
     [SerializeField] bool flipY;
     [SerializeField] bool checkGround = false;
@@ -22,6 +25,6 @@ public class PlayerMove : MonoBehaviour
         // If PlayerGroundCheck is on the same gameObject and checkGround is enabled, check if player is grounded
         if (gc != null && checkGround && !gc.isGrounded) return;
         // Moves player depending on Input Axis
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * (flipX ? -1 : 1), 0, Input.GetAxis("Vertical") * (flipY ? -1 : 1)) * playerSpeed * Time.deltaTime;
+        transform.position += new Vector3(moveDirections != moveMode.yOnly ? Input.GetAxis("Horizontal") * (flipX ? -1 : 1) : 0, 0, moveDirections != moveMode.xOnly ? Input.GetAxis("Vertical") * (flipY ? -1 : 1) : 0) * playerSpeed * Time.deltaTime;
     }
 }
