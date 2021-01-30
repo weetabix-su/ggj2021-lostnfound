b42e48ef022d167d25a870cd18fced3e958fa337
hkjason <66897406+hkjason@users.noreply.github.com>
2021-01-30 20:39:37 +0800

    level 1

---
+++ Assets/Scripts/AI/PoliceBehind.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PoliceBehind : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public bool actived = false;
+
+
+    /// <summary>
+    /// Should Be Implemented In Character, Inheriting
+    /// </summary>
+    public float movementSpeed;
+    public Animator animator;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        //agent.speed = GetMovementSpeed();
+        agent.speed = movementSpeed;
+    }
+
+    private void Start()
+    {
+        Queue.OnEnd += SetActived;
+    }
+
+    private void OnEnable()
+    {
+        actived = false;
+    }
+
+    private void OnDestroy()
+    {
+        Queue.OnEnd -= SetActived;
+    }
+
+    private void Update()
+    {
+        if (actived)
+        {
+            //Check for sight and attack range
+            playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+            //if (!_isDead)
+            //{
+            if (!playerInSightRange && !playerInAttackRange) Idling();
+            if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+            if (playerInAttackRange && playerInSightRange) AttackPlayer();
+            //}
+
+            //animator.SetFloat("Speed", agent.velocity.magnitude / agent.speed);
+        }
+    }
+
+    private void Idling()
+    {
+        return;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        actived = false;
+        LevelController.instance.LevelRetry();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SetActived();
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    private void SetActived()
+    {
+        actived = true;
+    }
+}
---
+++ Assets/Scripts/AI/QueueBehind.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class QueueBehind : MonoBehaviour
+{
+    [Header("Settings")]
+    public float waitingTime;
+    public int numberOfHumans;
+    public float walkingSpeed;
+
+    [HideInInspector]
+    public List<Transform> humans;
+    [HideInInspector]
+    public List<NavMeshAgent> agents;
+    [HideInInspector]
+    public List<Vector3> locations;
+
+    public bool activate = false;
+
+    private void Awake()
+    {
+        activate = false;
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfHumans; i++)
+        {
+            humans.Add(transform.GetChild(i));
+            agents.Add(humans[i].GetComponent<NavMeshAgent>());
+            agents[i].speed = walkingSpeed;
+        }
+        for (int i = numberOfHumans; i < numberOfHumans * 2; i++)
+        {
+            locations.Add(transform.GetChild(i).transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        if (activate)
+        {
+            StartSet();
+        }
+    }
+
+
+    void StartQueue()
+    {
+        for (int i = 0; i < agents.Count; i++)
+        {
+            agents[i].SetDestination(locations[i]);
+        }
+        activate = true;
+    }
+
+    void StartSet()
+    {
+        List<Vector3> temp = Queue.instance.GetLocations();
+        for (int i = 0; i < agents.Count; i++)
+        {
+            if (agents[i] == null)
+            {
+                continue;
+            }
+            // Check if we've reached the destination
+            if (!agents[i].pathPending)
+            {
+                if (agents[i].remainingDistance <= agents[i].stoppingDistance)
+                {
+                    if (!agents[i].hasPath || agents[i].velocity.sqrMagnitude == 0f)
+                    {
+                        agents[i].SetDestination(temp[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartQueue();
+        }
+    }
+}
