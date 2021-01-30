73293fa699411dacae120e65430975ecb833148d
hkjason <66897406+hkjason@users.noreply.github.com>
2021-01-30 23:18:39 +0800

    QueuePrefab

---
+++ Assets/Scripts/AI/LightScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    public Vector3 originalPosition;
+    public Transform reference;
+    public float diff;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+        diff = reference.position.x - originalPosition.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (reference == null)
+            return;
+        transform.position = new Vector3(reference.position.x - diff, originalPosition.y, originalPosition.z);
+    }
+}
