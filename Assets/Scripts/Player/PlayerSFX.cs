8616f2ec8c24ac59b12c559af6ebb35d0d66c847
Angelo Barredo <aibarredo@gmail.com>
2021-01-30 23:30:06 +0800

    Attempt fix on Level 3 environment box colliders

---
+++ Assets/Scripts/Player/PlayerSFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlayerSFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
