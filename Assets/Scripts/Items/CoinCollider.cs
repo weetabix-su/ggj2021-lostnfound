5ed30d365c02c89f7922cd361cdff8c7c2924a1d
Angelo Barredo <aibarredo@gmail.com>
2021-01-30 21:47:17 +0800

    Push attempt again after making changes without pulling. Silly me!

---
+++ Assets/Scripts/Items/CoinCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollider : MonoBehaviour
+{
+    public delegate void EnterCoin();
+    public static event EnterCoin OnEnterCoin;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (OnEnterCoin != null)
+        {
+            if(other.tag == "Player")
+                OnEnterCoin();
+        }
+    }
+}
