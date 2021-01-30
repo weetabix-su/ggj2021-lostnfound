255e0c3517eea1a819ec400f474b732bf951a945
hanglch <46513241+hanglch@users.noreply.github.com>
2021-01-30 15:55:06 +0800

    god particle push

---
+++ Assets/Scripts/AI/God.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class God : MonoBehaviour
+{
+    public float movingDegree;
+
+    public float localSpawnPosY;
+
+    private void Awake()
+    {
+        localSpawnPosY = this.transform.localPosition.y;
+    }
+
+    public void Start()
+    {
+        GodMove(true);
+    }
+
+    public void GodMove(bool isUp)
+    {
+        if (isUp)
+            LeanTween.moveLocalY(this.gameObject, localSpawnPosY + movingDegree, 2.5f).setEase(LeanTweenType.easeOutSine).setOnComplete(()=> { GodMove(false); });
+        else
+            LeanTween.moveLocalY(this.gameObject, localSpawnPosY - movingDegree, 2.5f).setEase(LeanTweenType.easeOutSine).setOnComplete(() => { GodMove(true); });
+    }
+
+}
