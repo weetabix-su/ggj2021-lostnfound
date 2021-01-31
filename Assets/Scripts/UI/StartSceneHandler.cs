505f1fa41e58eb95f1848288ace72ec9a2a4a567
Angelo Barredo <aibarredo@gmail.com>
2021-01-31 13:28:57 +0800

    Added start scene and splash screen elements

---
+++ Assets/Scripts/UI/StartSceneHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneHandler : MonoBehaviour
+{
+    [SerializeField] string GameSceneName = "MainScene";
+    Animator anim;
+    float clock;
+
+    IEnumerator startGame()
+    {
+        anim.SetBool("isStart", true);
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(GameSceneName);
+        yield return null;
+    }
+
+    void Start()
+    {
+        clock = 0;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (clock >= 2f)
+        {
+            if (Input.anyKeyDown)
+            {
+                StartCoroutine(startGame());
+            }
+        }
+        else clock += Time.deltaTime;
+    }
+}
